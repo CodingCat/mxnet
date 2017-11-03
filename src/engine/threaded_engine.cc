d06c4afd4d7de3b6ae4125af4dd5e06769fb29e8
CodingCat <zhunansjtu@gmail.com>
2017-11-02 21:04:06 -0700

    update

--- src/engine/threaded_engine.cc
+++ src/engine/threaded_engine.cc
@@ -388,7 +388,10 @@ void ThreadedEngine::WaitForVar(VarHandle var) {
 void ThreadedEngine::WaitForAll() {
   BulkFlush();
   std::unique_lock<std::mutex> lock{finished_m_};
+  std::cout << "waiting for all";
   finished_cv_.wait(lock, [this]() {
+      std::cout << pending_ << "\n";
+      std::this_thread::sleep_for(std::chrono::milliseconds(2000));
       return pending_.load() == 0 || kill_.load();
     });
 }
